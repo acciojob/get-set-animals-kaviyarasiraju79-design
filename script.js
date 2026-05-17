a86a662f9a69aa7ed39617df1bc6aafeca2916e0
Kaviyarasi Raju <kaviyarasiraju79@gmail.com>
2026-05-17 22:42:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,14 +13,19 @@ class Animal {
   }
 }
 
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
 class Cat extends Animal {
   purr() {
     console.log("purr");
   }
 }
 
-class Dog extends Animal {
-  bark() {
-    console.log("woof");
-  }
-}
+// Do not change the code below this line
+window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
